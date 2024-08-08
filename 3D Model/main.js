072ffd443b96ab96b92eadb8ff069ab446d77672
Wilord20 <edgartorres08@live.com.mx>
2024-08-07 18:03:43 -0600

    Camera controls were added

--- 3D Model/main.js
+++ 3D Model/main.js
@@ -1,15 +1,19 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas') });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(new THREE.Color(0xffffff)); // hex for white
-
 const loader = new GLTFLoader();
-
 let shiba;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(new THREE.Color(0xffffff)); // Hex for white color
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; // Make camera moves smoother
+
 loader.load(
     './shiba.glb', // Replace with the path to your Shiba model
     function (gltf) {
@@ -26,6 +30,7 @@ camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
+  controls.update(); // Update controls with damping
   renderer.render(scene, camera);
 }
 
